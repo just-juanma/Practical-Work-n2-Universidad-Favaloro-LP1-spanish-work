b80fee9fa17d04e6c1c665a7179f30e1fa5717e4
adriel levy <adriellevy2014@hotmail.com>
2022-04-28 21:07:35 -0300

    Cambios realizados para el main

---
+++ enc_temp_folder/eaf0332c319c8eb7d8e53c7c6c8e53b/cAeropuerto.h
@@ -0,0 +1,56 @@
+#ifndef _CAEROPUERTO_H
+#define _CAEROPUERTO_H
+#include "gbl.h"
+#include "cListaAvion.h"
+#include "cListaVuelo.h"
+
+class cAeropuerto {
+
+    #pragma region Friend classes
+
+    friend class cAvion;
+
+    #pragma endregion
+
+public: 
+
+    #pragma region Constructor y destructor
+
+        /// <summary>
+        /// Constructor por defecto parametrizado
+        /// </summary>
+        /// <param name="_ID"></param>
+        /// <param name="_capacidadAeropuerto"></param>
+        /// <param name="_nombre"></param>
+        cAeropuerto(string _ID=" ", unsigned int _capacidadAeropuerto=0, string _nombre="");
+
+        /// <summary>
+        /// Destructor por defecto
+        /// </summary>
+        ~cAeropuerto();
+
+    #pragma endregion
+
+    #pragma region metodos
+
+    bool darPermiso(cAvion* avion);
+    string to_string();
+    void imprimir();
+    ush cantAterrizados(cFecha* fecha);
+    ush cantDespegados(cFecha* fecha);
+    ush getcapacidadAeropuerto() { return this->capacidadAeropuerto; };
+    void setcapacidadAeropuerto(int _capacidad) { this->capacidadAeropuerto = _capacidad; }
+    void setListaVuelos(cListaVuelo* Lista) { this->listaVuelos = Lista; }
+   // float porcentajeHorarioPartida();
+
+    #pragma endregion
+
+private: 
+    const string ID;
+    const string nombre;
+    ush capacidadAeropuerto;
+    cListaVuelo* listaVuelos;
+    cListaAvion* hangar;
+};
+
+#endif // _CAEROPUERTO_H
